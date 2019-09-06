10803f27254f334338739347d49e29b1e77bf8ba
Andrei Zhydzenka <zhidenko_1999@mail.ru>
2019-09-06 19:36:46 +0300

    add calculation of estimations.

--- Lab1/Lab1/Form1.cs
+++ Lab1/Lab1/Form1.cs
@@ -12,6 +12,7 @@ namespace Lab1
 {
     public partial class Form1 : Form
     {
+        const int N = 100000;
         public Form1()
         {
             InitializeComponent();
@@ -25,7 +26,11 @@ namespace Lab1
                 double a = Convert.ToDouble(aTextBox.Text);
                 double R0 = Convert.ToDouble(R0TextBox.Text);
 
-                if(m <= a)
+                if(m > a)
+                {
+                    DoCalculations(m, a, R0);
+                }
+                else
                 {
                     MessageBox.Show("m should be greater than a!");
                 }
@@ -54,6 +59,49 @@ namespace Lab1
             }
 
             return result;
+        }
+
+        private void DoCalculations(double m, double a, double R0)
+        {
+            Generator gen = new Generator(a, m, R0);
+            List<double> xList = new List<double>();
+
+            foreach(double x in gen.GetNext().Take(N))
+            {
+                xList.Add(x);
+            }
+
+            double Mx, Dx, GAMMAx;
+
+            DoEstimationsCalculations(xList, out Mx, out Dx, out GAMMAx);
+            PrintEstimations(Mx, Dx, GAMMAx);
+        }
+
+        private void DoEstimationsCalculations(List<double> xList, out double Mx, out double Dx, out double GAMMAx)
+        {
+            double sum = 0, sumD = 0;
+
+            foreach(double x in xList)
+            {
+                sum += x;
+            }
+
+            Mx = sum / N;
+
+            foreach (double x in xList)
+            {
+                sumD += (x - Mx) * (x - Mx);
+            }
+
+            Dx = sumD / N;
+            GAMMAx = Math.Sqrt(Dx);
+        }
+
+        private void PrintEstimations(double Mx, double Dx, double GAMMAx)
+        {
+            MxLabel.Text = Math.Round(Mx, 4).ToString();
+            DxLabel.Text = Math.Round(Dx, 4).ToString();
+            GAMMAxLabel.Text = Math.Round(GAMMAx, 4).ToString();
         }
     }
 }
