c22495487652bd5ba0e574cb7a091f13272aa7ab
Andrei Zhydzenka <zhidenko_1999@mail.ru>
2019-09-06 13:44:25 +0300

    add validation of input data.

--- Lab1/Lab1/Form1.cs
+++ Lab1/Lab1/Form1.cs
@@ -17,5 +17,43 @@ namespace Lab1
             InitializeComponent();
         }
 
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            if(IsValid(mTextBox.Text) && IsValid(aTextBox.Text) && IsValid(R0TextBox.Text))
+            {
+                double m = Convert.ToDouble(mTextBox.Text);
+                double a = Convert.ToDouble(aTextBox.Text);
+                double R0 = Convert.ToDouble(R0TextBox.Text);
+
+                if(m <= a)
+                {
+                    MessageBox.Show("m should be greater than a!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("All the values should be positive!");
+            }
+        }
+
+        private bool IsValid(string text)
+        {
+            bool result = true;
+            double number;
+
+            if (double.TryParse(text, out number))
+            {
+                if(number <= 0)
+                {
+                    result = false;
+                }
+            }
+            else
+            {
+                result = false;
+            }
+
+            return result;
+        }
     }
 }
