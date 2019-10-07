c430b05f45be4635b10603d1ad43858bf94bce44
Andrei Zhydzenka <zhidenko_1999@mail.ru>
2019-10-07 21:54:38 +0300

    add lab3. Simulation of queue network.

---
+++ Lab3/Lab3/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace Lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.mTextBox = new System.Windows.Forms.TextBox();
+            this.aTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.R0TextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pi2TextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pi1TextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.ALabel = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.LLabel = new System.Windows.Forms.Label();
+            this.PLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.Green;
+            this.button1.Location = new System.Drawing.Point(356, 436);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(390, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "SIMULATE";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Green;
+            this.label1.Location = new System.Drawing.Point(93, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "m";
+            // 
+            // mTextBox
+            // 
+            this.mTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.mTextBox.Location = new System.Drawing.Point(166, 24);
+            this.mTextBox.Name = "mTextBox";
+            this.mTextBox.Size = new System.Drawing.Size(116, 38);
+            this.mTextBox.TabIndex = 2;
+            this.mTextBox.Text = "61121";
+            // 
+            // aTextBox
+            // 
+            this.aTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.aTextBox.Location = new System.Drawing.Point(516, 27);
+            this.aTextBox.Name = "aTextBox";
+            this.aTextBox.Size = new System.Drawing.Size(116, 38);
+            this.aTextBox.TabIndex = 4;
+            this.aTextBox.Text = "211";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Green;
+            this.label2.Location = new System.Drawing.Point(443, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 31);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "a";
+            // 
+            // R0TextBox
+            // 
+            this.R0TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.R0TextBox.Location = new System.Drawing.Point(860, 27);
+            this.R0TextBox.Name = "R0TextBox";
+            this.R0TextBox.Size = new System.Drawing.Size(116, 38);
+            this.R0TextBox.TabIndex = 6;
+            this.R0TextBox.Text = "319";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.Green;
+            this.label3.Location = new System.Drawing.Point(787, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 31);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "R0";
+            // 
+            // pi2TextBox
+            // 
+            this.pi2TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.pi2TextBox.Location = new System.Drawing.Point(646, 102);
+            this.pi2TextBox.Name = "pi2TextBox";
+            this.pi2TextBox.Size = new System.Drawing.Size(116, 38);
+            this.pi2TextBox.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.Green;
+            this.label4.Location = new System.Drawing.Point(574, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 31);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "pi2";
+            // 
+            // pi1TextBox
+            // 
+            this.pi1TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.pi1TextBox.Location = new System.Drawing.Point(356, 102);
+            this.pi1TextBox.Name = "pi1TextBox";
+            this.pi1TextBox.Size = new System.Drawing.Size(116, 38);
+            this.pi1TextBox.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.Color.Green;
+            this.label5.Location = new System.Drawing.Point(283, 105);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 31);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "pi1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.ForeColor = System.Drawing.Color.Green;
+            this.label6.Location = new System.Drawing.Point(467, 226);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 31);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "A";
+            // 
+            // ALabel
+            // 
+            this.ALabel.AutoSize = true;
+            this.ALabel.BackColor = System.Drawing.Color.Transparent;
+            this.ALabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ALabel.ForeColor = System.Drawing.Color.Green;
+            this.ALabel.Location = new System.Drawing.Point(598, 226);
+            this.ALabel.Name = "ALabel";
+            this.ALabel.Size = new System.Drawing.Size(29, 31);
+            this.ALabel.TabIndex = 12;
+            this.ALabel.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.ForeColor = System.Drawing.Color.Green;
+            this.label8.Location = new System.Drawing.Point(467, 340);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(73, 31);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Pотк";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.ForeColor = System.Drawing.Color.Green;
+            this.label9.Location = new System.Drawing.Point(467, 280);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(58, 31);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Lоч";
+            // 
+            // LLabel
+            // 
+            this.LLabel.AutoSize = true;
+            this.LLabel.BackColor = System.Drawing.Color.Transparent;
+            this.LLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LLabel.ForeColor = System.Drawing.Color.Green;
+            this.LLabel.Location = new System.Drawing.Point(598, 280);
+            this.LLabel.Name = "LLabel";
+            this.LLabel.Size = new System.Drawing.Size(29, 31);
+            this.LLabel.TabIndex = 15;
+            this.LLabel.Text = "0";
+            // 
+            // PLabel
+            // 
+            this.PLabel.AutoSize = true;
+            this.PLabel.BackColor = System.Drawing.Color.Transparent;
+            this.PLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PLabel.ForeColor = System.Drawing.Color.Green;
+            this.PLabel.Location = new System.Drawing.Point(598, 340);
+            this.PLabel.Name = "PLabel";
+            this.PLabel.Size = new System.Drawing.Size(29, 31);
+            this.PLabel.TabIndex = 16;
+            this.PLabel.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(1133, 522);
+            this.Controls.Add(this.PLabel);
+            this.Controls.Add(this.LLabel);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.ALabel);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.pi1TextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pi2TextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.R0TextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.aTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.mTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox mTextBox;
+        private System.Windows.Forms.TextBox aTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox R0TextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox pi2TextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox pi1TextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label ALabel;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label LLabel;
+        private System.Windows.Forms.Label PLabel;
+    }
+}
+
---
+++ Lab3/Lab3/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3
+{
+    public partial class Form1 : Form
+    {
+        const int ticksCount = 100000;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (IsValid(mTextBox.Text) && IsValid(aTextBox.Text) && IsValid(R0TextBox.Text) && IsValidDouble(pi1TextBox.Text) && IsValidDouble(pi2TextBox.Text))
+            {
+                double pi1 = Convert.ToDouble(pi1TextBox.Text);
+                double pi2 = Convert.ToDouble(pi2TextBox.Text);
+
+                if (pi1 <= 1 || pi2 <= 1)
+                {
+                    int m = Convert.ToInt32(mTextBox.Text);
+                    int a = Convert.ToInt32(aTextBox.Text);
+                    int R0 = Convert.ToInt32(R0TextBox.Text);
+
+                    if (m > a)
+                    {
+                        DoSimulation(m, a, R0, pi1, pi2);
+                    }
+                    else
+                    {
+                        MessageBox.Show("m should be greater than a!");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Pi1 and Pi2 should be less than 1!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("All the values should be positive integers!");
+            }
+        }
+
+        private bool IsValid(string text)
+        {
+            bool result = true;
+            int number;
+
+            if (int.TryParse(text, out number))
+            {
+                if (number <= 0)
+                {
+                    result = false;
+                }
+            }
+            else
+            {
+                result = false;
+            }
+
+            return result;
+        }
+
+        private bool IsValidDouble(string text)
+        {
+            bool result = true;
+            double number;
+
+            if (double.TryParse(text, out number))
+            {
+                if (number <= 0)
+                {
+                    result = false;
+                }
+            }
+            else
+            {
+                result = false;
+            }
+
+            return result;
+        }
+
+        private void DoSimulation(int m, int a, int R0, double pi1, double pi2)
+        {
+            Generator gen = new Generator(a, m, R0);
+            int taskCount = 0, doneTaskCount = 0, deletedTaskCount = 0;
+            int inputStatus = 2, queueStatus = 0, channel1Status = 0, channel2Status = 0;
+            int queueLengthSum = 0;
+
+            for(var i = 1; i <= ticksCount; i++)
+            {
+                if (channel2Status == 1) // if channel #2 has a task
+                {                        // then check 
+                    if (gen.GetNext() <= pi2) // if event will happen
+                    {                         // if happens then
+                        doneTaskCount++;      // increase done task counter
+                        channel2Status = 0;   // and make channel free
+                    }
+                }
+
+                if(channel1Status == 1) // if channel #2 has a task
+                {                       // then check
+                    if (gen.GetNext() <= pi1) // if event will happen
+                    {                           // if happens then
+                        channel1Status = 0;     // and make channel free
+                        if (channel2Status == 0)  //check if channel #2 is free
+                        {                         // if it's free
+                            channel2Status = 1;   // put task in channel #2 and make it busy
+                        }
+                        else                     // if channel #2 is busy
+                        {
+                            deletedTaskCount++;  // then delete task and increase deleted task counter
+                        }
+                    }
+                }
+
+                if(queueStatus > 0)
+                {
+                    if(channel1Status == 0)
+                    {
+                        channel1Status = 1;
+                        queueStatus--;
+                    }
+                }
+
+                if(inputStatus == 1)
+                {
+                    taskCount++;
+                    if (channel1Status == 0)
+                    {
+                        channel1Status = 1;
+                    }
+                    else
+                    {
+                        if (queueStatus < 2)
+                        {
+                            queueStatus++;
+                        }
+                        else
+                        {
+                            deletedTaskCount++;
+                        }
+                    }
+                }
+
+                queueLengthSum += queueStatus;
+
+                if (inputStatus == 2)
+                {
+                    inputStatus = 1;
+                }
+                else
+                {
+                    inputStatus = 2;
+                }
+            }
+
+            PLabel.Text = ((double)deletedTaskCount / (deletedTaskCount + doneTaskCount)).ToString();
+            ALabel.Text = ((double)doneTaskCount / ticksCount).ToString();
+            LLabel.Text = ((double)queueLengthSum / ticksCount).ToString();
+        }
+    }
+}
